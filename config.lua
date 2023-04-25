9754844141169184da8501558f82a21e12892a56
Ilias Rbayti <fivedscripts@gmail.com>
2023-04-25 15:02:05 +0200

    v1.0.0

---
+++ config.lua
@@ -0,0 +1,14 @@
+_CONFIG = 
+{
+    bagItem = "backpack",
+    bagInventoryWeight = 250, -- inventory weight that will be set when bag is equipped
+    forceBackpack = -- forces player to wear backpack when item is in inventory.
+    {
+        enabled = true,
+        backpack = 
+        {
+            bags_1 = 60,
+            bags_2 = 0
+        }
+    }
+}
